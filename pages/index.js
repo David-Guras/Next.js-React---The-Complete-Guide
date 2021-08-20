41c22ec240d49f6420eaab9e916b13cb67905664
David Guras <gura.san@gmail.com>
2021-08-19 22:48:11 -0400

    add dummy data-set add 3 dummy images add component/event-item add component/event-list add component/event-list to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
 import Link from "next/link";
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 function Homepage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Homepage</h1>
-      <ul>
-        <li>
-          <Link href="/events/event-1">Featured Event 1</Link>
-        </li>
-        <li>
-          <Link href="/events/event-2">Featured Event 2</Link>
-        </li>
-        <li>
-          <Link href="/events/event-3">Featured Event 3</Link>
-        </li>
-        <li>
-          <Link href="/events/event-4">Featured Event 4</Link>
-        </li>
-      </ul>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
