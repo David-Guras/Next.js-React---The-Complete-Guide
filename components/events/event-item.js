793c4c064718bcd1f62c10ff0c79f5caf15d083a
David Guras <gura.san@gmail.com>
2021-08-21 13:20:35 -0400

    add svg icons to event-item component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,10 @@
-import Link from "next/link";
 import Button from "../ui/button";
 import classes from "./event-item.module.css";
+
+// icon import
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -23,13 +27,20 @@ function EventItem(props) {
           <h2>{title}</h2>
         </div>
         <div className={classes.date}>
+          <DateIcon />
           <time>{formattedDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
