3851ed2caccfd07bb3981f7c7dececd5a698ba11
David Guras <gura.san@gmail.com>
2021-08-22 14:29:26 -0400

    add event-search module and styles add event-search to events index page add simple button logic to buttons component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import { Fragment } from 'react'
 import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
 
 function AllEventsPage() {
   const allEvents = getAllEvents();
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={allEvents} />
-    </div>
+    </Fragment>
   );
 }
 
